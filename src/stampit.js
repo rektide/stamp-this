af12fd62d906bd6f20f93399ccbbdd396bf09054
rektide <rektide@voodoowarez.com>
2015-07-05 02:24:54 -0400

    Check to see if `this` is a real thing, if so, mix it in during the factory call.

--- src/stampit.js
+++ src/stampit.js
@@ -119,7 +119,10 @@ const stampit = function stampit(options) {
   }
 
   const factory = function Factory(refs, ...args) {
-    let instance = mixin(create(fixed.methods), fixed.refs, refs);
+    let isGlobal = this === global || this === factory;
+    let instance = isGlobal ?
+      mixin(create(fixed.methods), fixed.refs, refs) :
+      mixin(this, create(fixed.methods), fixed.refs, refs);
     mergeUnique(instance, fixed.props); // props are safely merged into refs
 
     let nextPromise = null;
